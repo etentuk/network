1ae59d00b3fe2e5191fe7d63c1946a482c4a84e8
Etentuk <etentuke@gmail.com>
2021-12-06 01:42:48 +0100

    users can now create new posts

--- network/views.py
+++ network/views.py
@@ -1,10 +1,13 @@
+import json
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+
+from .models import Post, User
 
 
 def index(request):
@@ -61,3 +64,19 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+@login_required
+def post(request):
+    # Creating a new post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+    data = json.loads(request.body)
+    print(data)
+    post = data.get("post", "")
+    new_post = Post(post=post, author=request.user)
+    new_post.save()
+    return JsonResponse({"message": "Post created successfully"})
+
+
+
